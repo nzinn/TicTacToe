c16f3245ccd55153e25294ad45da1014aedefc08
Beckham-jr <apaysnoe@gmail.com>
2022-05-09 10:34:47 -0500

    Im a complete print_winner function.

--- tictactoe.lua
+++ tictactoe.lua
@@ -24,6 +24,14 @@ end
 
 
 function print_winner(player)
+  
+  if player == 1 then
+    print("player one won please stand on the podium")
+  elseif player == 2 then
+    print("player two won please stand on the podium")
+  end
+  
+
 
 end
 
