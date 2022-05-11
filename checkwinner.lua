4e7307504c5a51e1a08efd5ecaf59c330dcd8965
Noah Zinn <noahwzinn@gmail.com>
2022-05-11 10:48:15 -0500

    added checks for row 1 and column 1

--- checkwinner.lua
+++ checkwinner.lua
@@ -1,1 +1,24 @@
-local board = {{" ", "x", "o"}, {"x", "o", " "}, {"o", "x", " "}}
+local board = {{"o", "o", "x"}, {"x", "o", " "}, {"o", "x", " "}}
+
+
+
+
+
+
+
+
+
+if board[1][1] == "o" and board[1][2] == "o" and board [1][3] == "o"   then
+   print("Row 1 has been won by o")
+else
+   print("nobody has won row one")
+end
+
+
+
+if board[1][1] == "o" and board[2][1] == "o" and board[3][1] == "o" then
+   print("o has won column 1")
+end
+
+
+
