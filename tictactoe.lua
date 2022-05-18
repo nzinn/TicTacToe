0de70cf0fcd614645e44498a3475f7906f5e24ec
Noah Zinn <noahwzinn@gmail.com>
2022-05-18 12:06:35 -0500

    finished tic tac toe

--- tictactoe.lua
+++ tictactoe.lua
@@ -1,5 +1,5 @@
 
-local new_board = {{"x", "x", "x"}, {" ", "o", " "}, {"x", "o", " "}}
+--local new_board = {{"x", "x", "x"}, {" ", "o", " "}, {"x", "o", " "}}
 
 function print_instructions()
   print("this is tic tac toe you can either be x or o,\n if you get three x's or o's in a row you win")
@@ -8,30 +8,67 @@ function print_instructions()
   
 function put_x(board, row, col)
    if board[row][col] == " " then
-          board[row][col] = "x"
-       end
+      board[row][col] = "x"
+      return true
+   else
+      return false
+   end
 end
 
 function put_o(board, row, col)
   if board[row][col] == " " then
-    board[row][col] = "o"
+     board[row][col] = "o"
+     return true
+  else
+     return false
   end
-end
+ end
 
 
 function play_turn(board, is_x_turn)
-   print("player " .. player_turn .. ", it's you're turn!")
 
-   print("input row! (1 2 3)")
-   local row = io.read()
+   --clear the screen
+   print("\027[H\027[2J")
+
+   print_board(board)
+
+   local player
    
-   print("input column! (1 2 3)")
-   local column = io.read()
+   if is_x_turn then
+      player = "x"
+   else
+      player = "o"
+   end
+   
+   
+   print("player " .. player  .. ", it's you're turn!")
+
+   print("input row! (1, 2, or 3)")
+   local row = io.read("*n")
+
+   
+   print("\027[H\027[2J")
+
+   print_board(board)
+
+   print("player " .. player  .. ", it's you're turn!")
+   
+   print("input column! (1, 2, or 3)")
+   local column = io.read("*n")
+
 
    if is_x_turn then
-      put_x(board, row, column)
+
+      --keep trying until they give a valid play
+      if not put_x(board, row, column) then
+	 print("Space is not empty, please try again!")
+	 play_turn(board, is_x_turn)
+      end
    else
-      put_o(board, row, column)
+      if not put_o(board, row, column) then
+	 print("Space is not empty, please try again!")
+	 play_turn(board, is_x_turn)
+      end
    end   
 end
 
@@ -45,7 +82,7 @@ function check_winner(board)
       
       winner = true
       
-      for col  = 2, 3 do
+      for col = 2, 3 do
 	 if board[row][col - 1] ~= board[row][col] then
 	    winner = false
 	 end
@@ -93,21 +130,34 @@ end
 
 function print_winner(player)
   
-  if player == 1 then
-    print("player one won please stand on the podium")
-  elseif player == 2 then
-    print("player two won please stand on the podium")
+  if player == "x"  then
+    print("x won please stand on the podium")
+  elseif player == "o" then
+    print("o won please stand on the podium")
   end
-  
-
-
 end
 
 
 
 
 function print_board(board)
+
+   --print column numbers
+   io.write("  ")
+
+   for col =1, 3 do
+      io.write(col .. " ")
+   end
+
+   print()
+
+   
+   
    for row  = 1, 3 do
+
+      --print row numbers
+      io.write(row .. " ")
+      
       for col = 1, 3 do
 	 io.write(board[row][col])
 	 if col ~= 3 then
@@ -118,7 +168,7 @@ function print_board(board)
       io.write("\n")
       
       if row ~= 3 then
-	 io.write("-----")
+	 io.write("  -----")
       end
       
       io.write("\n")
@@ -134,33 +184,39 @@ end
 local is_xplayer_turn = true
 
 
-local game_over = true
+local game_over = false
 
-print_board(new_board)
+--initialize board
+local board = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}}
 
-print(check_winner(new_board))
---[[
+
+--clear the screen
+print("\027[H\027[2J")
+
 while not game_over do
 
 
    
-   play_turn(new_board, is_xplayer_turn)
+   play_turn(board, is_xplayer_turn)
 
+   --switch turns
    is_xplayer_turn = not is_xplayer_turn
    
 
 
    --returns "x" if player one wins, "o" if player 2, and false if nobody wins
-   game_over = check_winner()
-
+   game_over = check_winner(board)
 
 end
 
+print("\027[H\027[2J")
+
+print_board(board)
 
 print_winner(game_over)
 
 print("goodbye!")
 
---]]
+
 
 
